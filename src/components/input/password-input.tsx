a7c4a749bebe1d72afe18313f66ecf5aab538098
Wallace Viana <wallaceviana@Wallaces-MacBook-Pro.local>
2025-12-11 16:25:11 -0300

    fix: password field size

--- src/components/input/password-input.tsx
+++ src/components/input/password-input.tsx
@@ -1,18 +1,21 @@
-import { type ComponentProps, useCallback, useState } from "react"
-import { Eye, EyeOff } from "lucide-react"
+import { type ComponentProps, useCallback, useState } from "react";
+import { Eye, EyeOff } from "lucide-react";
 
-import { Input } from "./input"
-import { cn } from "../../lib/utils"
+import { Input } from "./input";
+import { cn } from "../../lib/utils";
 
-export function PasswordInput({ className,...props }: ComponentProps<"input">) {
-  const [visible, setVisible] = useState<boolean>(false)
+export function PasswordInput({
+  className,
+  ...props
+}: ComponentProps<"input">) {
+  const [visible, setVisible] = useState<boolean>(false);
 
   const handleToggleVisibility = useCallback(() => {
-    setVisible(current => !current)
-  }, [])
-  
+    setVisible((current) => !current);
+  }, []);
+
   return (
-    <div className="relative">
+    <div className="relative w-full">
       <Input
         {...props}
         data-slot="input"
@@ -24,9 +27,9 @@ export function PasswordInput({ className,...props }: ComponentProps<"input">) {
         className="absolute right-0 top-0 size-10 flex items-center justify-center"
         onClick={handleToggleVisibility}
       >
-        {visible && <EyeOff className="size-4 text-gray-300"/>}
+        {visible && <EyeOff className="size-4 text-gray-300" />}
         {!visible && <Eye className="size-4 text-gray-300" />}
       </button>
     </div>
-  )
+  );
 }
