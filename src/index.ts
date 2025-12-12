102873737f343c8d2b2d631eca8ebe1f261e0435
Christianvilla26 <Christianvilla26@hotmail.com>
2025-12-12 17:55:47 -0400

    feat: add Menu component with dropdown functionality and associated s… (#2)
    * feat: add Menu component with dropdown functionality and associated stories and tests
    
    * feat: introduce SuspendedMenu component with dropdown functionality, including stories and tests
    
    ---------
    
    Co-authored-by: christianVillaPlayonSports <christian.villa@playonsports.com>

--- src/index.ts
+++ src/index.ts
@@ -15,3 +15,4 @@ export * from "./components/table";
 export * from "./components/textarea";
 export * from "./components/typography";
 export * from "./components/image-upload";
+export * from "./components/suspended-menu";
