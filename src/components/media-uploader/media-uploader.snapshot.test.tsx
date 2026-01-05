61c2721d1e756f47a6f1638b0efc590ec8f03d05
Lorenzo Lopez Olliver <lorenzo.olliver@Lorenzos-MacBook-Air.local>
2026-01-05 13:00:36 -0600

    last miniute fixes

--- src/components/media-uploader/media-uploader.snapshot.test.tsx
+++ src/components/media-uploader/media-uploader.snapshot.test.tsx
@@ -1,10 +1,12 @@
 import { describe, it, expect } from "vitest";
 import { render } from "@testing-library/react";
-import MediaUploader from "./media-uploader";
+import MediaUploader, { type MediaInfo } from "./media-uploader";
 
 describe("MediaUploader snapshot", () => {
   it("matches snapshot (default)", () => {
-    const { container } = render(<MediaUploader value={null} />);
+    const { container } = render(<MediaUploader value={null} onChange={function (_value: MediaInfo): void {
+      throw new Error("Function not implemented.");
+    } } />);
     expect(container).toMatchSnapshot();
   });
 });
