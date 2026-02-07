dc71fef300d94c9a01c01f275d11ba50d67cb31c
Wallace Viana <wallaceviana@Wallaces-MacBook-Pro.local>
2026-02-07 14:45:42 -0300

    fix: spacing in section component

--- dist/mercyseat-ui.es.js
+++ dist/mercyseat-ui.es.js
@@ -11400,27 +11400,24 @@ function fb({
     }
   );
 }
-const Ih = Hn(
-  "bg-white rounded-2xl overflow-hidden flex flex-col w-full",
-  {
-    variants: {
-      elevation: {
-        "elevation-0": "shadow-none",
-        "elevation-1": "shadow-[0px_1px_1px_rgba(0,0,0,0.14),0px_2px_1px_-1px_rgba(0,0,0,0.2)]",
-        "elevation-2": "shadow-[0px_2px_2px_rgba(0,0,0,0.14),0px_3px_1px_-2px_rgba(0,0,0,0.2)]",
-        "elevation-3": "shadow-[0px_3px_4px_rgba(0,0,0,0.14),0px_3px_3px_-2px_rgba(0,0,0,0.2)]",
-        "elevation-4": "shadow-[0px_4px_5px_rgba(0,0,0,0.14),0px_2px_4px_-1px_rgba(0,0,0,0.2)]",
-        "elevation-5": "shadow-[0px_5px_8px_rgba(0,0,0,0.14),0px_3px_5px_-1px_rgba(0,0,0,0.2)]",
-        "elevation-6": "shadow-[0px_6px_10px_rgba(0,0,0,0.14),0px_3px_5px_-1px_rgba(0,0,0,0.2)]",
-        "elevation-7": "shadow-[0px_7px_10px_1px_rgba(0,0,0,0.14),0px_4px_5px_-2px_rgba(0,0,0,0.2)]",
-        "elevation-8": "shadow-[0px_8px_10px_1px_rgba(0,0,0,0.14),0px_5px_5px_-3px_rgba(0,0,0,0.2)]"
-      }
-    },
-    defaultVariants: {
-      elevation: "elevation-1"
+const Ih = Hn("bg-white rounded-2xl overflow-hidden flex flex-col w-full", {
+  variants: {
+    elevation: {
+      "elevation-0": "shadow-none",
+      "elevation-1": "shadow-[0px_1px_1px_rgba(0,0,0,0.14),0px_2px_1px_-1px_rgba(0,0,0,0.2)]",
+      "elevation-2": "shadow-[0px_2px_2px_rgba(0,0,0,0.14),0px_3px_1px_-2px_rgba(0,0,0,0.2)]",
+      "elevation-3": "shadow-[0px_3px_4px_rgba(0,0,0,0.14),0px_3px_3px_-2px_rgba(0,0,0,0.2)]",
+      "elevation-4": "shadow-[0px_4px_5px_rgba(0,0,0,0.14),0px_2px_4px_-1px_rgba(0,0,0,0.2)]",
+      "elevation-5": "shadow-[0px_5px_8px_rgba(0,0,0,0.14),0px_3px_5px_-1px_rgba(0,0,0,0.2)]",
+      "elevation-6": "shadow-[0px_6px_10px_rgba(0,0,0,0.14),0px_3px_5px_-1px_rgba(0,0,0,0.2)]",
+      "elevation-7": "shadow-[0px_7px_10px_1px_rgba(0,0,0,0.14),0px_4px_5px_-2px_rgba(0,0,0,0.2)]",
+      "elevation-8": "shadow-[0px_8px_10px_1px_rgba(0,0,0,0.14),0px_5px_5px_-3px_rgba(0,0,0,0.2)]"
     }
+  },
+  defaultVariants: {
+    elevation: "elevation-1"
   }
-);
+});
 function mb({
   title: e,
   children: n,
@@ -11429,18 +11426,11 @@ function mb({
   className: o,
   ...i
 }) {
-  return /* @__PURE__ */ l.jsxs(
-    "section",
-    {
-      ...i,
-      className: z(Ih({ elevation: r, className: o })),
-      children: [
-        !!e && /* @__PURE__ */ l.jsx("header", { className: "h-15 flex items-center px-8 border-b border-b-gray-25", children: /* @__PURE__ */ l.jsx("h3", { className: "font-sans font-semibold text-base text-gray-900 leading-5", children: e }) }),
-        /* @__PURE__ */ l.jsx("div", { className: "flex flex-col p-4 md:p-8", children: n }),
-        !!t && /* @__PURE__ */ l.jsx("footer", { className: "px-4 md:px-8 py-4 md:py-6 border-t border-t-gray-25", children: t })
-      ]
-    }
-  );
+  return /* @__PURE__ */ l.jsxs("section", { ...i, className: z(Ih({ elevation: r, className: o })), children: [
+    !!e && /* @__PURE__ */ l.jsx("header", { className: "h-15 flex items-center px-8 border-b border-b-gray-25", children: /* @__PURE__ */ l.jsx("h3", { className: "font-sans font-semibold text-base text-gray-900 leading-5", children: e }) }),
+    /* @__PURE__ */ l.jsx("div", { className: "flex flex-col p-4 md:p-8!", children: n }),
+    !!t && /* @__PURE__ */ l.jsx("footer", { className: "px-4 md:px-8! py-4 md:py-6! border-t border-t-gray-25", children: t })
+  ] });
 }
 var xc = {
   color: void 0,
--- src/components/section/section.tsx
+++ src/components/section/section.tsx
@@ -5,35 +5,24 @@ import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "../../lib/utils";
 
-const sectionVariants = cva(
-  "bg-white rounded-2xl overflow-hidden flex flex-col w-full",
-  {
-    variants: {
-      elevation: {
-        "elevation-0": "shadow-none",
-        "elevation-1":
-          "shadow-[0px_1px_1px_rgba(0,0,0,0.14),0px_2px_1px_-1px_rgba(0,0,0,0.2)]",
-        "elevation-2":
-          "shadow-[0px_2px_2px_rgba(0,0,0,0.14),0px_3px_1px_-2px_rgba(0,0,0,0.2)]",
-        "elevation-3":
-          "shadow-[0px_3px_4px_rgba(0,0,0,0.14),0px_3px_3px_-2px_rgba(0,0,0,0.2)]",
-        "elevation-4":
-          "shadow-[0px_4px_5px_rgba(0,0,0,0.14),0px_2px_4px_-1px_rgba(0,0,0,0.2)]",
-        "elevation-5":
-          "shadow-[0px_5px_8px_rgba(0,0,0,0.14),0px_3px_5px_-1px_rgba(0,0,0,0.2)]",
-        "elevation-6":
-          "shadow-[0px_6px_10px_rgba(0,0,0,0.14),0px_3px_5px_-1px_rgba(0,0,0,0.2)]",
-        "elevation-7":
-          "shadow-[0px_7px_10px_1px_rgba(0,0,0,0.14),0px_4px_5px_-2px_rgba(0,0,0,0.2)]",
-        "elevation-8":
-          "shadow-[0px_8px_10px_1px_rgba(0,0,0,0.14),0px_5px_5px_-3px_rgba(0,0,0,0.2)]",
-      },
+const sectionVariants = cva("bg-white rounded-2xl overflow-hidden flex flex-col w-full", {
+  variants: {
+    elevation: {
+      "elevation-0": "shadow-none",
+      "elevation-1": "shadow-[0px_1px_1px_rgba(0,0,0,0.14),0px_2px_1px_-1px_rgba(0,0,0,0.2)]",
+      "elevation-2": "shadow-[0px_2px_2px_rgba(0,0,0,0.14),0px_3px_1px_-2px_rgba(0,0,0,0.2)]",
+      "elevation-3": "shadow-[0px_3px_4px_rgba(0,0,0,0.14),0px_3px_3px_-2px_rgba(0,0,0,0.2)]",
+      "elevation-4": "shadow-[0px_4px_5px_rgba(0,0,0,0.14),0px_2px_4px_-1px_rgba(0,0,0,0.2)]",
+      "elevation-5": "shadow-[0px_5px_8px_rgba(0,0,0,0.14),0px_3px_5px_-1px_rgba(0,0,0,0.2)]",
+      "elevation-6": "shadow-[0px_6px_10px_rgba(0,0,0,0.14),0px_3px_5px_-1px_rgba(0,0,0,0.2)]",
+      "elevation-7": "shadow-[0px_7px_10px_1px_rgba(0,0,0,0.14),0px_4px_5px_-2px_rgba(0,0,0,0.2)]",
+      "elevation-8": "shadow-[0px_8px_10px_1px_rgba(0,0,0,0.14),0px_5px_5px_-3px_rgba(0,0,0,0.2)]",
     },
-    defaultVariants: {
-      elevation: "elevation-1",
-    },
-  }
-);
+  },
+  defaultVariants: {
+    elevation: "elevation-1",
+  },
+});
 
 type SectionProps = ComponentProps<"section"> &
   VariantProps<typeof sectionVariants> & {
@@ -51,22 +40,15 @@ export function Section({
   ...props
 }: SectionProps) {
   return (
-    <section
-      {...props}
-      className={cn(sectionVariants({ elevation, className }))}
-    >
+    <section {...props} className={cn(sectionVariants({ elevation, className }))}>
       {!!title && (
         <header className="h-15 flex items-center px-8 border-b border-b-gray-25">
-          <h3 className="font-sans font-semibold text-base text-gray-900 leading-5">
-            {title}
-          </h3>
+          <h3 className="font-sans font-semibold text-base text-gray-900 leading-5">{title}</h3>
         </header>
       )}
-      <div className="flex flex-col p-4 md:p-8">{children}</div>
+      <div className="flex flex-col p-4 md:p-8!">{children}</div>
       {!!footer && (
-        <footer className="px-4 md:px-8 py-4 md:py-6 border-t border-t-gray-25">
-          {footer}
-        </footer>
+        <footer className="px-4 md:px-8! py-4 md:py-6! border-t border-t-gray-25">{footer}</footer>
       )}
     </section>
   );
